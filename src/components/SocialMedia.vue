6b4e0541f1ac33f115e303fb91a5d96493515443
Marichka Offen <marichka.offen@gmail.com>
2020-01-09 11:10:11 -0500

    Work on Social Media: - add sprite svg for social media logos; - set up Socia Media vue component.

---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <a href="https://www.facebook.com/marichka.offen" target="_blank">
+      <svg class="footer__icon">
+        <use xlink:href="../assets/img/social_media_logos.svg#facebook" />
+      </svg>
+    </a>
+    <a href="https://www.linkedin.com/in/marichka-offen" target="_blank">
+      <svg class="footer__icon">
+        <use xlink:href="../assets/img/social_media_logos.svg#linkedin" />
+      </svg>
+    </a>
+    <a href="https://twitter.com/MarichkaOffen" target="_blank">
+      <svg class="footer__icon">
+        <use xlink:href="../assets/img/social_media_logos.svg#twitter" />
+      </svg>
+    </a>
+    <a href="https://github.com/marichka-offen" target="_blank">
+      <svg class="footer__icon">
+        <use xlink:href="../assets/img/social_media_logos.svg#github" />
+      </svg>
+    </a>
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
