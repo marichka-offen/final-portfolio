a1052d4b746d699587e11e39b5ed0e09a3623c25
Marichka Offen <marichka.offen@gmail.com>
2020-01-09 12:22:02 -0500

    Style Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="title">
+    <span class="title__first">Marichka</span>
+    <span class="title__last">Offen</span>
+    <span class="title__sub">Front End Developer</span>
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+span {
+  display: block;
+  text-transform: uppercase;
+}
+
+.title {
+  &__first {
+    font-size: 5rem;
+  }
+
+  &__last {
+    font-size: 9.5rem;
+    margin-top: -2.4rem;
+  }
+
+  &__sub {
+    font-size: 2.8rem;
+    font-weight: 200;
+  }
+}
+</style>
