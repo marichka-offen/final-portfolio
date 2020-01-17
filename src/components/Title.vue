922a66491ceb43ca8cfd4d8e2d7b0a4dc1d13301
Marichka Offen <marichka.offen@gmail.com>
2020-01-17 17:15:33 -0500

    Update Title component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="title">
     <span class="title__first">Marichka</span>
-    <span class="title__last heading--stroke heading--shadow">Offen</span>
+    <span class="title__last">Offen</span>
     <span class="title__sub">Front End Developer</span>
   </h1>
 </template>
@@ -13,6 +13,16 @@ span {
 }
 
 .title {
+  @mixin stroke-width($property) {
+    -webkit-text-stroke-width: $property;
+    -moz-text-stroke-width: $property;
+  }
+
+  @mixin stroke-color($property) {
+    -webkit-text-stroke-color: $property;
+    -moz-text-stroke-color: $property;
+  }
+
   &__first {
     font-size: 5rem;
   }
@@ -20,37 +30,16 @@ span {
   &__last {
     font-size: 9.5rem;
     margin-top: -2.4rem;
+    letter-spacing: 6px;
+    @include stroke-width(2px);
+    @include stroke-color(#0d1b1e);
+    color: transparent;
+    text-shadow: 6px 6px #c0ca33;
   }
 
   &__sub {
     font-size: 2.8rem;
     font-weight: 200;
-  }
-}
-
-@mixin stroke-width($property) {
-  -webkit-text-stroke-width: $property;
-  -moz-text-stroke-width: $property;
-}
-
-@mixin stroke-color($property) {
-  -webkit-text-stroke-color: $property;
-  -moz-text-stroke-color: $property;
-}
-
-.heading {
-  font-size: 8rem;
-  letter-spacing: 6px;
-  margin: 2rem 0;
-
-  &--stroke {
-    @include stroke-width(2px);
-    @include stroke-color(#0d1b1e);
-    color: transparent;
-  }
-
-  &--shadow {
-    text-shadow: 6px 6px #c0ca33;
   }
 }
 </style>
