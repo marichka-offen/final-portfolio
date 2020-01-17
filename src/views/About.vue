80390cabe9b884787717a447f77ceaca90d2b057
Marichka Offen <marichka.offen@gmail.com>
2020-01-17 17:16:50 -0500

    Rewrite About component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,63 +1,131 @@
 <template>
   <div class="about">
-    <h2>
-      A Little About Me
-    </h2>
+    <h2>A Little About Me</h2>
     <p>
-      I am a web development bootcamp graduate, programming aficionado and
-      learning enthusiast. This website is a quintessence of my knowledge,
-      library of the efforts made, and diary of personal growth. I am front-end
-      devotee and enjoy all things React and Vue, but do not limit myself. I am
-      constantly learning new ways of creating and improving websites and web
-      apps. Currently I am working as a freelancer, occasionaly volunteer and
-      contribute to open source. My most recent contribution was to help with
-      translation of ReactJS documentation to Ukrainian and Russian to share the
-      knowledge all across the world.
+      As funny as it sounds, my career in web development started with a game.
+      It was winter of 2018, and I was bored and jobless (promise, it's not
+      another deadbeat story, I just wasn't able to obtain my work permit at the
+      time). Anyways, I was scrolling through the Internet trying to find
+      something to do and absolutely randomly found
+      <a
+        href="https://codecombat.com/"
+      >this web-game</a> where you move your
+      character by writing pieces of code in Python. It just so happened that at
+      the time the creators of the game released additional paid levels, which
+      were teaching players the basics of HTML programming. My curiosity won
+      over my snobbish nature, and I dove deep into a funny small tutorial. I
+      couldn't even imagine that a timekiller for a day would turn into a
+      passion.
     </p>
-    <h3>Skills:</h3>
-    <ul>
-      <li>
-        Highly skilled in creating perfomant
-        <span class="highlight">HTML</span> and
-        <span class="highlight">CSS</span>
-      </li>
-      <li>
-        Passionate about <span class="highlight">CSS Methodologies</span>,
-        <span class="highlight">CSS Preprocessors</span>,
-        <span class="highlight">Animation</span> and
-        <span class="highlight">SVGs</span>
-      </li>
-      <li>
-        Experienced with <span class="highlight">JavaScript</span>,
-        <span class="highlight">React</span> and
-        <span class="highlight">Vue</span>,
-        <span class="highlight">Node</span> and
-        <span class="highlight">Express</span>,
-        <span class="highlight">State Management</span>
-      </li>
-      <li>
-        Proficient with <span class="highlight">Task Runners</span>,
-        <span class="highlight">Package Managers</span> and
-        <span class="highlight">Version Control Systems</span>
-      </li>
-      <li>
-        Knowledgeable about <span class="highlight">User Experience</span>,
-        <span class="highlight">Accessibility</span>,
-        <span class="highlight">Technical Writing</span>,
-        <span class="highlight">Performance</span> and
-        <span class="highlight">Responsive Web Development</span>
-      </li>
-      <li>
-        Worked with <span class="highlight">Python</span>,
-        <span class="highlight">Django</span>,
-        <span class="highlight">MongoDB</span> and
-        <span class="highlight">PostgreSQL</span>
-      </li>
-    </ul>
+    <p>
+      I fell in love with web development and in a short time found myself
+      devouring HTML, CSS and JavaScript courses on
+      <a
+        href="https://www.edx.org/"
+      >Edx</a>,
+      <a href="https://www.codecademy.com/">Codecademy</a> and
+      <a href="https://www.freecodecamp.org/">FreeCodeCamp</a> (I'm leaving
+      links here not for advertising purposes, but as starting point for those
+      who would like a starting point. All of the resources are absolutely
+      free). My excitement and devotion to my new hobby was so deep and powerful
+      that I made an important life decision afterwards, I decided to do
+      whatever it takes to become a web developer. Shortly after I found myself
+      a student of Web Development Immersive program in the
+      <a
+        href="https://generalassemb.ly/"
+      >General Assembly</a> school.
+    </p>
+    <p>
+      It was a constant fight with myself about what was more important to me:
+      my knowledge, career, success, happiness or the need to relax and
+      procrastinate. I would be lying if I said that I didn&#39;t get
+      discouraged at times. Sometimes I felt lost and sometimes – absolutely
+      imposturous. But as any developer, I just couldn&#39;t give up the feeling
+      of exuberance every time I solved the problem (developers&#39; high is
+      real!).
+    </p>
+    <p>
+      Around the time I graduated school I discovered another absolutely amazing
+      resource for self-learners –
+      <a href="https://www.udemy.com/">Udemy</a>,
+      which became my Netflix 24/7.
+    </p>
+    <p>
+      I am still learning, but this sums of my progress so far. I fell in love
+      with programming because with all its constant changes, it gave me an
+      opportunity to tame my thirst for knowledge. I fell in love with many
+      wonderfully people I met in this amazing community, forever dreamers,
+      and(delete) go-getters, and life-changers. And(delete) Best of all I was
+      finally able to find the feeling of accomplishment that I have been
+      craving for so long.
+      <br />
+      <br />The End!
+      <br />
+      <br />P.S. I am currently learning Adobe Illustrator and planning to take
+      the
+      <a
+        href="https://edex.adobe.com/en/aca"
+      >Adobe Certified Associate exam</a>
+      to add graphic design to my skillset!
+    </p>
+
+    <div class="skills">
+      <!-- <img
+        src="../assets/img/blocks.svg"
+        alt="animation of lego blocks consequently descending from the top to represent the building up of knowledge"
+        class="img"
+      />-->
+      <h3>Skills:</h3>
+      <ul>
+        <li>
+          Highly skilled in creating perfomant
+          <span class="highlight">HTML</span>and
+          <span class="highlight">CSS</span>
+        </li>
+        <li>
+          Passionate about
+          <span class="highlight">CSS Methodologies</span>,
+          <span class="highlight">CSS Preprocessors</span>,
+          <span class="highlight">Animation</span>and
+          <span class="highlight">SVGs</span>
+        </li>
+        <li>
+          Experienced with
+          <span class="highlight">JavaScript</span>,
+          <span class="highlight">React</span>and
+          <span class="highlight">Vue</span>,
+          <span class="highlight">Node</span>and
+          <span class="highlight">Express</span>,
+          <span class="highlight">State Management</span>
+        </li>
+        <li>
+          Proficient with
+          <span class="highlight">Task Runners</span>,
+          <span class="highlight">Package Managers</span>and
+          <span class="highlight">Version Control Systems</span>
+        </li>
+        <li>
+          Knowledgeable about
+          <span class="highlight">User Experience</span>,
+          <span class="highlight">Accessibility</span>,
+          <span class="highlight">Technical Writing</span>,
+          <span class="highlight">Performance</span>and
+          <span class="highlight">Responsive Web Development</span>
+        </li>
+        <li>
+          Worked with
+          <span class="highlight">Python</span>,
+          <span class="highlight">Django</span>,
+          <span class="highlight">MongoDB</span>and
+          <span class="highlight">PostgreSQL</span>
+        </li>
+      </ul>
+    </div>
 
     <h3>Education:</h3>
     <ul class="education">
       <li class="education__wrap">
+        <h4>Taras Shevchenko National University of Kyiv | Institute of Philology</h4>
         <p>
           In 2016 I obtained BA in linguistics and communication, major English,
           Literature and Translation. Learnt to analyse language form, language
@@ -69,8 +137,9 @@
         </p>
       </li>
       <li class="education__wrap">
+        <h4>Udemy</h4>
         <p>
-          I am an avid learner, and spent more then 200 hours on Udemy teaching
+          I am an avid learner, and spent hours and hours on Udemy teaching
           myself new technologies and perfecting existing knowledge. That being
           said, I do not give up easily when facing a problem or a subject that
           I am not familiar with, and I am strongly determined to find the right
@@ -78,6 +147,8 @@
         </p>
       </li>
       <li class="education__wrap">
+        <h4>General Assembly</h4>
+
         <p>
           I have successfuly completed a 3-month, 500-hour full-time and
           full-stack web development program in General Assembly DC. That being
@@ -89,36 +160,17 @@
   </div>
 </template>
 
+<script>
+import "../assets/img/blocks.svg";
+export default {};
+</script>
+
 <style lang="scss">
 ul {
-  line-height: 2;
+  line-height: 1.8;
+  min-width: min-content;
 }
-.highlight {
-  display: inline-block;
-  color: #c0ca33;
-  font-weight: 600; //   padding: 0 5px;
-}
-
-.education {
-  list-style-type: none;
-  width: 90%;
-  margin: 0 auto;
-  &__wrap {
-    padding: 0.4rem;
-    margin-bottom: 2rem;
-    border-radius: 5px;
-    background-image: linear-gradient(
-      to bottom right,
-      rgba(#dce775, 0.8),
-      rgba(#c0ca33, 0.8),
-      rgba(#9e9d24, 0.8)
-    );
-
-    & > p {
-      padding: 1rem;
-      background-color: #fff;
-      margin: 0;
-    }
-  }
+h4 {
+  margin: 0;
 }
 </style>
